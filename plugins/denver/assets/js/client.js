5209fa5a94633dd58647588263cdc755ef8e6726
C9 <matt@c9.io>
2012-09-29 18:34:05 -0400

    New

--- plugins/denver/assets/js/client.js
+++ plugins/denver/assets/js/client.js
@@ -1,5 +1,9 @@
 var socket = io.connect();
 
+$(document).ready(function() {
+    $("#chat_input").focus();
+});
+
 socket.on("connect", function() {
     $("#chat_input").removeAttr("disabled");
 });
@@ -9,7 +13,24 @@ socket.on("notice", function(msg) {
 });
 
 socket.on("chat", function(msg) {
-    // Output this somewhere. I wonder where!
+    var outStr = '<div class="chat_msg"><span class="username">' + msg.id + 
+        ':</span>\t\t' + msg.msg + '</div>'
+    $("#chat_output_container").append(outStr);
 });
+
+function processInput(ev, el) {
+    if (ev.keyCode === 13) {
+        socket.emit("chat", el.value);
+        el.value = "";
+    }
+}
+
+function earthquake() {
+    $(".chat_msg").removeClass("wave").addClass("earthquake");
+}
+
+function wave() {
+    $(".chat_msg").removeClass("earthquake").addClass("wave");
+}
 
 // To send a message, socket.emit("chat", chat_input.value);
